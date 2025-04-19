752d71f162c53d8d524654f8aeb3471eefe6e14b
dextrux <titolucas1@live.com>
2025-04-19 11:29:45 -0300

    Explosive Barrel implementation
    Implementação base do barriel explosivo. Criação do prefab base do barril explosivo

---
+++ Assets/Scripts/Obstacles/ExplosiveBarrel.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using UnityEngine;
+
+public class ExplosiveBarrel : ResetableBase {
+    [SerializeField] private float _timeToExplode;
+    [SerializeField] private GameObject _visual;
+    private bool _willExplodePlayer;
+    private bool _wasExploded;
+
+    private void Awake() {
+        _wasExploded = false;
+        _willExplodePlayer = false;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (!_wasExploded) {
+            StartCoroutine(ExplosionAfterColdown());
+            _willExplodePlayer = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (!_wasExploded) {
+            _willExplodePlayer = false;
+        }
+    }
+
+    public override void ResetObject() {
+        base.ResetObject();
+
+        _visual.SetActive(true);
+        _willExplodePlayer = false;
+        _wasExploded = false;
+    }
+
+    IEnumerator ExplosionAfterColdown() {
+        yield return new WaitForSeconds(_timeToExplode);
+        _visual.SetActive(false);
+        if (_willExplodePlayer) {
+            RespawnController.OnPlayerRespawn.Invoke();
+        }
+    }
+}
