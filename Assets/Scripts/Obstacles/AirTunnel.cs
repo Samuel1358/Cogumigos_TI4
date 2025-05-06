4cf7eb6e2a9d7dbcf20ca47f8cd5bacedecfeb1e
Samuel1358 <bazonsamuel@gmail.com>
2025-05-06 17:01:20 -0300

    Update AirTunnel.cs

--- Assets/Scripts/Obstacles/AirTunnel.cs
+++ Assets/Scripts/Obstacles/AirTunnel.cs
@@ -23,6 +23,62 @@ public class AirTunnel : MonoBehaviour {
     private float _segmentDuration;
     private float _segmentProgress;
 
+
+    private void Awake() {
+        if (_moveByTime) {
+            _journeyLength = 0f;
+            for (int i = 0; i < _pathPoints.Count - 1; i++) {
+                _journeyLength += Vector3.Distance(_pathPoints[i].position, _pathPoints[i + 1].position);
+            }
+
+            _calculatedSpeed = _journeyLength / _totalTime;
+        }
+        SetupNextSegment();
+    }
+
+    void Update() {
+        if (_isActive) {
+            _segmentProgress += Time.deltaTime;
+
+            float t = _segmentProgress / _segmentDuration;
+            _objToMove.position = Vector3.Slerp(_startPoint, _targetPoint, t);
+
+            if (_segmentProgress >= _segmentDuration) {
+                _objToMove.position = _targetPoint;
+                SetupNextSegment();
+            }
+        }
+    }
+
+    // Private Methods
+    private void ResetTunnel()
+    {
+        _isActive = false;
+        player.SetGlide(false);
+        _objToMove = null;
+        _currentIndex = 0;
+        _segmentProgress = 0f;
+    }
+
+    private void SetupNextSegment() 
+    {
+        if (_currentIndex >= _pathPoints.Count - 1) {
+            _currentIndex = _pathPoints.Count;
+            ResetTunnel();
+            return;
+        }
+
+        _startPoint = _pathPoints[_currentIndex].position;
+        _targetPoint = _pathPoints[_currentIndex + 1].position;
+        _segmentDistance = Vector3.Distance(_startPoint, _targetPoint);
+
+        float currentSpeed = _moveByTime ? _calculatedSpeed : _speed;
+        _segmentDuration = _segmentDistance / currentSpeed;
+        _segmentProgress = 0f;
+
+        _currentIndex++;
+    }
+
     private void OnTriggerEnter(Collider collider) {
         if (collider.transform.parent.TryGetComponent<Player>(out player)) {
             Debug.Log("Colided player");
@@ -40,55 +96,21 @@ public class AirTunnel : MonoBehaviour {
         }
     }
 
-    private void Awake() {
-        if (_moveByTime) {
-            _journeyLength = 0f;
-            for (int i = 0; i < _pathPoints.Count - 1; i++) {
-                _journeyLength += Vector3.Distance(_pathPoints[i].position, _pathPoints[i + 1].position);
+    // Gizmo
+    private void OnDrawGizmosSelected()
+    {
+        Vector3? oldVec = null;
+        foreach (var pathPoint in _pathPoints)
+        {
+            if (oldVec == null)
+            {
+                oldVec = pathPoint.position;
             }
-
-            _calculatedSpeed = _journeyLength / _totalTime;
+            else
+            {
+                Gizmos.DrawLine(oldVec.Value, pathPoint.position);
+                oldVec = pathPoint.position;
+            }           
         }
-        SetupNextSegment();
-    }
-
-    private void ResetTunnel() {
-        _isActive = false;
-        player.SetGlide(false);
-        _objToMove = null;
-        _currentIndex = 0;
-        _segmentProgress = 0f;
-    }
-
-    void Update() {
-        if (_isActive) {
-            _segmentProgress += Time.deltaTime;
-
-            float t = _segmentProgress / _segmentDuration;
-            _objToMove.position = Vector3.Slerp(_startPoint, _targetPoint, t);
-
-            if (_segmentProgress >= _segmentDuration) {
-                _objToMove.position = _targetPoint;
-                SetupNextSegment();
-            }
-        }
-    }
-
-    void SetupNextSegment() {
-        if (_currentIndex >= _pathPoints.Count - 1) {
-            _currentIndex = _pathPoints.Count;
-            ResetTunnel();
-            return;
-        }
-
-        _startPoint = _pathPoints[_currentIndex].position;
-        _targetPoint = _pathPoints[_currentIndex + 1].position;
-        _segmentDistance = Vector3.Distance(_startPoint, _targetPoint);
-
-        float currentSpeed = _moveByTime ? _calculatedSpeed : _speed;
-        _segmentDuration = _segmentDistance / currentSpeed;
-        _segmentProgress = 0f;
-
-        _currentIndex++;
     }
 }
