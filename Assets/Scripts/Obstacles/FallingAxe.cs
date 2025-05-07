09386abab976091ba11e618b9af4398c7704d3e6
Arthur <86436882+arthurmordente@users.noreply.github.com>
2025-05-07 13:01:50 -0300

    Falling axe and Rolling Trunk

---
+++ Assets/Scripts/Obstacles/FallingAxe.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class FallingAxe : MonoBehaviour
+{
+    [SerializeField] private float fallSpeed = 300f;
+    [SerializeField] private float waitTime = 3f;
+
+    private float currentWaitTime = 0f;
+    private Quaternion startRotation;
+    private Quaternion targetRotation;
+    private bool isWaiting = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        startRotation = transform.rotation;
+        targetRotation = startRotation * Quaternion.Euler(0, 0, 90f);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (isWaiting)
+        {
+            currentWaitTime += Time.deltaTime;
+            if (currentWaitTime >= waitTime)
+            {
+                isWaiting = false;
+                currentWaitTime = 0f;
+            }
+        }
+        else
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, fallSpeed * Time.deltaTime);
+            
+            if (Quaternion.Angle(transform.rotation, targetRotation) < 0.1f)
+            {
+                isWaiting = true;
+                targetRotation = (targetRotation == startRotation) ? 
+                    startRotation * Quaternion.Euler(0, 0, 90f) : 
+                    startRotation;
+            }
+        }
+    }
+}
