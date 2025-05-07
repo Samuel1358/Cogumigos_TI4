2ddc4a74922bd656ac2efe9df4baf9da8684d15b
Samuel1358 <bazonsamuel@gmail.com>
2025-05-07 15:53:33 -0300

    ajuste

--- Assets/Scripts/Obstacles/FallingAxe.cs
+++ Assets/Scripts/Obstacles/FallingAxe.cs
@@ -10,14 +10,12 @@ public class FallingAxe : MonoBehaviour
     private Quaternion targetRotation;
     private bool isWaiting = false;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
         startRotation = transform.rotation;
         targetRotation = startRotation * Quaternion.Euler(0, 0, 90f);
     }
 
-    // Update is called once per frame
     private void Update()
     {
         if (isWaiting)
