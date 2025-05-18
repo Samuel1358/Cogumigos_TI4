6308f922441297ec6cef8b12f859de40b2454d2d
Arthur <86436882+arthurmordente@users.noreply.github.com>
2025-05-18 14:45:47 -0300

    DeathFloor adjustment

--- Assets/Scripts/Player/BlobShadow.cs
+++ Assets/Scripts/Player/BlobShadow.cs
@@ -12,15 +12,15 @@ public class BlobShadow : MonoBehaviour
     {
         Ray downRay = new Ray(new Vector3(this.transform.position.x, this.transform.position.y - offset, this.transform.position.z), -Vector3.up);
 
-        //gets the hit from the raycast and converts it unto a vector3
+
         Vector3 hitPosition = hit.point;
         //transofrm the shadow to the location
         shadow.transform.position = hitPosition;
 
-        //Cast a ray straight downwards, reads back where it lands (this is optional but reccomended)
+
         if(Physics.Raycast(downRay, out hit))
         {
-            print(hit.transform);
+
         }
     }
 }
