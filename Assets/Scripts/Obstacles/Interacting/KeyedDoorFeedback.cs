62b7a188a6b1f9f279c0c29e1861d913035ceec4
Samuel1358 <bazonsamuel@gmail.com>
2025-06-26 17:07:38 -0300

    b

--- Assets/Scripts/Obstacles/Interacting/KeyedDoorFeedback.cs
+++ Assets/Scripts/Obstacles/Interacting/KeyedDoorFeedback.cs
@@ -2,12 +2,12 @@ using System;
 using UnityEngine;
 using DG.Tweening;
 
-public class KeyedDoorFeedback : MonoBehaviour
+public class KeyedDoorFeedback : ResetableBase
 {
     [Header("External Accesses")]
     [SerializeField] private Transform _lockSpot;
-    [SerializeField] private GameObject _keyModel;
-    [SerializeField, Min(0.1f)] private float _keyModelScale;
+    [SerializeField] private GameObject _keyVisual;
+    //[SerializeField, Min(0.1f)] private float _keyVisualScale;
 
     [Header("Settings")]
     [SerializeField] private Ease _easeBase = Ease.Linear;
@@ -16,19 +16,30 @@ public class KeyedDoorFeedback : MonoBehaviour
     [SerializeField] private float _finalRotateSpeed;
     [SerializeField] private float _finalRotateDuration;
 
+    private bool _activeAtCheckpoint;
+
+    private void Start()
+    {
+        _keyVisual.SetActive(false);
+        //_keyVisual.transform.localScale *= _keyVisualScale;
+
+        RespawnController.OnPlayerChangeCheckPoint += SetMemory;
+    }
+
     public void Open(Collider other, Action openAction)
     {
-        GameObject key = Instantiate(_keyModel, other.transform.position, Quaternion.identity);
-        key.transform.localScale *= _keyModelScale;
+        _keyVisual.transform.position = other.transform.position;
+        _keyVisual.transform.rotation = Quaternion.identity;
+        _keyVisual.SetActive(true);
 
         if (_rotateOnMoveDuration > _moveDuration)
             _rotateOnMoveDuration = _moveDuration;
 
-        Tween move = DOTween.To(() => key.transform.position, x => key.transform.position = x, _lockSpot.position, _moveDuration).SetEase(_easeBase);
-        Tween rotateOnMove = DOTween.To(() => key.transform.localEulerAngles, x => key.transform.localEulerAngles = x, _lockSpot.eulerAngles, _rotateOnMoveDuration).SetEase(_easeBase);
+        Tween move = DOTween.To(() => _keyVisual.transform.position, x => _keyVisual.transform.position = x, _lockSpot.position, _moveDuration).SetEase(_easeBase);
+        Tween rotateOnMove = DOTween.To(() => _keyVisual.transform.localEulerAngles, x => _keyVisual.transform.localEulerAngles = x, _lockSpot.localEulerAngles, _rotateOnMoveDuration).SetEase(_easeBase);
 
         Tween finalRotate = TweenHandler.Timer(_finalRotateDuration)
-            .OnUpdate(() => FinalRotate(key.transform));            
+            .OnUpdate(() => FinalRotate(_keyVisual.transform));            
 
         Sequence sequence = DOTween.Sequence()
             .Append(move)
@@ -39,5 +50,16 @@ public class KeyedDoorFeedback : MonoBehaviour
     private void FinalRotate(Transform transform)
     {
         transform.Rotate(_finalRotateSpeed * Time.deltaTime * Vector3.up, Space.Self);
+    }  
+
+    // Resetable
+    private void SetMemory(Checkpoint checkpoint)
+    {
+        _activeAtCheckpoint = _keyVisual.activeSelf;
+    }
+
+    public override void ResetObject()
+    {
+        _keyVisual.SetActive(_activeAtCheckpoint);
     }
 }
