09dd2f851a1d0b6412f0885f90b82bd271f4d462
Samuel1358 <bazonsamuel@gmail.com>
2025-06-27 14:59:27 -0300

    Colocando os barks
    Barks faltantes:
    CoguColect
    CoguUse
    Platform
    AirTunel
    Breakable
    Brazier
    Mirror

--- Assets/Scripts/Checkpoint/Checkpoint.cs
+++ Assets/Scripts/Checkpoint/Checkpoint.cs
@@ -6,12 +6,15 @@ public class Checkpoint : MonoBehaviour
 {
     [SerializeField] private int _coguGain;
     [SerializeField] private bool _canActive = true;
+    [SerializeField] private UnityEvent _onSetCheckpoint;
+
     private void OnTriggerEnter(Collider other) {
         if (other.transform.parent.TryGetComponent<Player>(out Player player)) {
             if (_canActive) {
                 player.SetCheckpoint(_coguGain);
                 RespawnController.Instance.SetActiveCheckPoint(this);
                 RespawnController.OnPlayerChangeCheckPoint.Invoke(this);
+                _onSetCheckpoint.Invoke();
                 _canActive = false;
             }
         }
