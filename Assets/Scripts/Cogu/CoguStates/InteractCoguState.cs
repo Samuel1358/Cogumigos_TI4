0b2b6ddb6da2440bcb5a84c94dce825c3d009fd9
Arthur <86436882+arthurmordente@users.noreply.github.com>
2025-07-06 13:59:06 -0300

    Mirror and Puzzle prefab

--- Assets/Scripts/Cogu/CoguStates/InteractCoguState.cs
+++ Assets/Scripts/Cogu/CoguStates/InteractCoguState.cs
@@ -14,7 +14,6 @@ public class InteractCoguState : CoguState
         //Debug.Log("Enter - Interact");
         _interacting += _stateMachine.Cogu.StartInteracting;
         _stateMachine.Cogu.ResetAnableCast();
-        GameIniciator.Instance.AudioManagerInstance.PlaySFX("CoguTransform");
 
         _stateMachine.Cogu.Animator.SetBool("Interact", true);
     }
