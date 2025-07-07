d8a2d43be2ead4f403950d523a0cf0337cde37dd
dextrux <titolucas1@live.com>
2025-07-07 18:54:28 -0300

    Level03 last

--- Assets/_menuCorrection/Finalsetter.cs
+++ Assets/_menuCorrection/Finalsetter.cs
@@ -5,7 +5,26 @@ public class Finalsetter : MonoBehaviour
     [SerializeField] private FinalDoor _leftDoor;
     [SerializeField] private FinalDoor _centerDoor;
     [SerializeField] private FinalDoor _rightDoor;
+    [SerializeField] private CollectablePageDialog _trueFinl;
+    [SerializeField] private CollectablePageDialog _fakeFinal;
+    [SerializeField] private PersistenteCollectableDataSO[] CollectableDataSOs;
+    private bool _goodFinal;
+
     private void OnTriggerEnter(Collider other) {
-        
+        _goodFinal = true;
+        foreach(PersistenteCollectableDataSO collectableBaseSO in CollectableDataSOs) {
+            if (!collectableBaseSO.VerifyCollected()) {
+                _goodFinal = false;
+            }
+        }
+        if (_goodFinal) {
+            _rightDoor.Activate();
+            _leftDoor.Activate();
+            _trueFinl.gameObject.SetActive(true);
+        }
+        else {
+            _centerDoor.Activate();
+            _fakeFinal.gameObject.SetActive(true);
+        }
     }
 }
